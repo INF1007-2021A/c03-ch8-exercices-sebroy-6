23f424aa0c4b3986f0265071b0687d9152d8d34e
sebroy-6 <sebastien-6.roy@polymtl.ca>
2021-11-08 13:39:22 -0500

    Modified/updated version

--- exercice.py
+++ exercice.py
@@ -16,10 +16,38 @@ def compareFichier(nom_fichier_reference, nom_fichier_a_comparer):
 
             for i in range(len(list_lines_r)):
                 for j in range(len(list_lines_r[i])):
-                     if list_lines_r[i][j] != list_lines_c[i][j]:
-                         return f"différence à la position {i}, {j}"
-                     else:
-                         return "Aucune différence"
+                    if len(list_lines_r) != len(list_lines_c):
+                        return f"différence à la position {i}, {j}"
+                    if len(list_lines_r[i]) != len(list_lines_c[i]):
+                        return f"différence de longueur à la ligne {i}"
+                    if list_lines_r[i][j] != list_lines_c[i][j]:
+                        return f"différence à la position {i}, {j}"
+
+            return "Aucune différence"
+
+def tripleSpaceFile(nom_fichier_a_tripler):
+    with open(nom_fichier_a_tripler) as r, open("Fichier_espaces_triple", "w") as t:
+        lines_r = r.readlines()
+
+        for i in range(len(lines_r)):
+            for j in lines_r[i]:
+                if j == " ":
+                    t.write(j*3)
+                else:
+                    t.write(j)
+
+def affectationNotes(nom_fichier_notes):
+    with open(nom_fichier_notes, "r") as r, open("notes_notees.txt", "w") as n:
+        list_grades = r.readlines()
+
+        for grade in list_grades:
+            grade_int = int(grade.strip())
+
+            for letter in PERCENTAGE_TO_LETTER:
+
+                if PERCENTAGE_TO_LETTER[letter][0] <= grade_int < PERCENTAGE_TO_LETTER[letter][1]:
+                    n.write(f"{grade_int} {letter}\n")
+
 
 # TODO: Définissez vos fonction ici
 
@@ -27,4 +55,5 @@ def compareFichier(nom_fichier_reference, nom_fichier_a_comparer):
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     print(compareFichier("./exemple.txt", "./exemple2"))
-    pass
+    tripleSpaceFile("./exemple.txt")
+    affectationNotes("./notes.txt")
